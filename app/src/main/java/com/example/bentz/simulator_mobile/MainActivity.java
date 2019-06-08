5a95a4dddb4344e09c5f416b014f67cd47407503
Benzion Rozen <benzion.rozen@live.biu.ac.il>
2019-06-08 13:07:15 +0300

    made main activity and connection to joystick activity

--- app/src/main/java/com/example/bentz/simulator_mobile/MainActivity.java
+++ app/src/main/java/com/example/bentz/simulator_mobile/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.bentz.simulator_mobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText  ip = (EditText) findViewById(R.id.ip_text);
+        final EditText port = (EditText) findViewById(R.id.port_text);
+        Button connect_button = (Button) findViewById(R.id.connect_button);
+        final Intent intent = new Intent(this,JoysticActivity.class);
+        connect_button.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+                        String ip_text= ip.getText().toString();
+                        String port_text = port.getText().toString();
+                        intent.putExtra("ip_text",ip_text);
+                        intent.putExtra("port_text",port_text);
+                        startActivity(intent);
+                    }
+                });
     }
 }
